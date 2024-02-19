566bb1dbf32771dca6c37c5ed1e9c5288c9bf16d
Adil <adilc0070@gmail.com>
2024-02-19 11:49:10 +0530

    route ready

--- routing/src/App.jsx
+++ routing/src/App.jsx
@@ -1,19 +1,22 @@
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import About from './Containers/about';
-import Profile from './Containers/profile';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import About from "./Containers/about";
+import Profile from "./Containers/profile";
 
 function App() {
   return (
-    <Router>
-      <div>
-        <Link to="/about"><button>About</button></Link>
-        <Link to="/profile"><button>Profile</button></Link>
-      </div>
+    <>
 
-      <Route component={About} path='/about' />
-      <Route component={Profile} path='/profile' />
-    </Router>
-  );
+      <Router>
+      <Link style={{marginLeft:"40px",color:"red", textDecoration:"none",}} to='/'>home</Link>
+      <Link style={{marginLeft:"40px",color:"red", textDecoration:"none",}} to='/pro'>profilePage</Link>
+      <Link style={{marginLeft:"40px",color:"red", textDecoration:"none",}} to='/about'>about</Link>
+        <Routes>
+          <Route Component={Profile} path="/pro" />
+          <Route Component={About} path="/about" />
+        </Routes>
+      </Router>
+    </>
+  )
 }
 
 export default App;
